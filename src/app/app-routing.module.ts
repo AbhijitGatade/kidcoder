8a9c3b0938a9b7154552935f0716eb5db994b346
Om <ombhandari82@gmail.com>
2022-06-30 19:19:41 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ClassesComponent } from './classes/classes.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
+import { TeachersComponent } from './teachers/teachers.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent}
+  {path:"", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"classes", component:ClassesComponent},
+  {path:"teachers", component:TeachersComponent},
+  {path:"gallery", component:GalleryComponent},
+  {path:"contact", component:ContactComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ClassesComponent } from './classes/classes.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ClassesComponent,
+    ContactComponent,
+    TeachersComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
